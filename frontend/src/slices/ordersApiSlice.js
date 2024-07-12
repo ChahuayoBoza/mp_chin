234cfa2fa2efc8e72efc1a473f75297e97c4e6dd
Chin <chahuayoboza@gmail.com>
2024-07-12 18:36:39 -0500

    pay with paypal fake

--- frontend/src/slices/ordersApiSlice.js
+++ frontend/src/slices/ordersApiSlice.js
@@ -16,19 +16,19 @@ export const orderApiSlice = apiSlice.injectEndpoints({
       }),
       keepUnusedDataFor: 5,
     }),
-    // payOrder: builder.mutation({
-    //   query: ({ orderId, details }) => ({
-    //     url: `${ORDERS_URL}/${orderId}/pay`,
-    //     method: 'PUT',
-    //     body: details,
-    //   }),
-    // }),
-    // getPaypalClientId: builder.query({
-    //   query: () => ({
-    //     url: PAYPAL_URL,
-    //   }),
-    //   keepUnusedDataFor: 5,
-    // }),
+    payOrder: builder.mutation({
+      query: ({ orderId, details }) => ({
+        url: `${ORDERS_URL}/${orderId}/pay`,
+        method: 'PUT',
+        body: {...details},
+      }),
+    }),
+    getPaypalClientId: builder.query({
+      query: () => ({
+        url: PAYPAL_URL,
+      }),
+      keepUnusedDataFor: 5,
+    }),
     // getMyOrders: builder.query({
     //   query: () => ({
     //     url: `${ORDERS_URL}/mine`,
@@ -53,8 +53,8 @@ export const orderApiSlice = apiSlice.injectEndpoints({
 export const {
   useCreateOrderMutation,
   useGetOrderDetailsQuery,
-  // usePayOrderMutation,
-  // useGetPaypalClientIdQuery,
+  usePayOrderMutation,
+  useGetPaypalClientIdQuery,
   // useGetMyOrdersQuery,
   // useGetOrdersQuery,
   // useDeliverOrderMutation,
